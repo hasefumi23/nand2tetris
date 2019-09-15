fc48960fef83a732ba9f9fba2a85940b4f88273c
hasegawa-f <hasegawa-f@east-inc.jp>
2019-09-15 19:25:45 +0900

    Complete chapter 4

--- nand2tetris/nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/nand2tetris/projects/04/fill/Fill.asm
@@ -11,4 +11,87 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// スクリーンの変数を設定
+@16384
+D=A
+@SCREEN
+M=D
+(LOOP_KEY)
+  // キーボードの変数を設定
+  @KBD
+  D=M
+  @pressed
+  M=D
+
+  @pressed
+  D=M
+
+  @ELSE_KEY
+  D;JNE
+
+  // 0 = white / 1 = black
+  @color
+  M=0
+  @DISP_SCREEN // スクリーンに表示する処理へジャンプ
+  0;JMP
+(ELSE_KEY)
+  @color
+  M=-1
+  @DISP_SCREEN // スクリーンに表示する処理へジャンプ
+  0;JMP
+(DISP_SCREEN)
+  @SCREEN
+  D=A
+  @screen
+  M=D
+
+  @i
+  M=0
+(LOOP_ROW)
+  @i
+  D=M
+  @256
+  D=D-A
+  @END_ROW
+  D;JGT
+  @i // i++
+  M=M+1
+  @j
+  M=0
+
+  (LOOP_COL)
+    @j
+    D=M
+    @32
+    // @5
+    D=D-A
+    @END_COL // if (j > 32)
+    D;JGT
+
+    // 指定された色を表示する
+    @color
+    D=M
+    @screen // 現在のスクリーンのアドレスの位置を指定
+    A=M
+    M=-1
+    M=D
+
+    // スクリーンの次のピクセルを指定する
+    @1
+    D=A
+    @screen
+    M=M+D
+
+    @j // j++
+    M=M+1
+
+    @LOOP_COL
+    0;JMP
+  (END_COL)
+    @LOOP_ROW
+    0;JMP
+(END_ROW)
+(END_KEY)
+
+@LOOP_KEY
+0;JMP
--- nand2tetris/nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// Computes sum=1+...+100.
+  @i // i=1
+  M=1
+  @sum // sum=0
+  M=0
+(LOOP)
+  @i // if (i-100)=0 goto END
+  D=M
+  @R1
+  D=D-M
+  @END
+  D;JGT
+  @R0 // sum+=i
+  D=M
+  @sum
+  M=D+M
+  @i // i++
+  M=M+1
+  @LOOP // goto LOOP
+  0;JMP
+(END) // infinite loop
+  @sum
+  D=M
+  @R2
+  M=D
+  @END
+  0;JMP
