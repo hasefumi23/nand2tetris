c20405e4c5a61fb18a6e6600d08b733a44bfdd6e
hasegawa-f <hasegawa-f@east-inc.jp>
2019-09-18 23:24:37 +0900

    Complete initial implementation of Code.rb

--- nand2tetris/nand2tetris/projects/06/Assembler/lib/code.rb
+++ nand2tetris/nand2tetris/projects/06/Assembler/lib/code.rb
@@ -1,18 +1,70 @@
 class Code
+  class CodeSyntaxError < StandardError; end
+
   class << self
     def dest(mnemonic)
       # dest ニーモニックのバイナリコードを返す
-      '000'
+      case mnemonic
+      when nil then "000"
+      when "M" then "001"
+      when "D" then "010"
+      when "MD" then "011"
+      when "A" then "100"
+      when "AM" then "101"
+      when "AD" then "110"
+      when "AMD" then "111"
+      else raise CodeSyntaxError
+      end
     end
 
     def comp(mnemonic)
       # comp ニーモニックのバイナリコードを返す
-      '0000000'
+      case mnemonic
+      when "0" then "0101010"
+      when "1" then "0111111"
+      when "-1" then "0111010"
+      when "D" then "0001100"
+      when "A" then "0110000"
+      when "!D" then "0001101"
+      when "!A" then "0110001"
+      when "-D" then "0001111"
+      when "-A" then "0110011"
+      when "D+1" then "0011111"
+      when "A+1" then "0110111"
+      when "D-1" then "0001110"
+      when "A-1" then "0110010"
+      when "D+A" then "0000010"
+      when "D-A" then "0010011"
+      when "A-D" then "0000111"
+      when "D&A" then "0000000"
+      when "D|A" then "0010101"
+      when "M" then "1110000"
+      when "!M" then "1110001"
+      when "-M" then "1110011"
+      when "M+1" then "1110111"
+      when "M-1" then "1110010"
+      when "D+M" then "1000010"
+      when "D-M" then "1010011"
+      when "M-D" then "1000111"
+      when "D&M" then "1000000"
+      when "D|M" then "1010101"
+      else raise CodeSyntaxError
+      end
     end
 
     def jump(mnemonic)
       #  jump ニーモニックのバイナリコードを返す
-      '000'
+      case mnemonic
+      when nil then "000"
+      when "JGT" then "001"
+      when "JEQ" then "010"
+      when "JGE" then "011"
+      when "JLT" then "100"
+      when "JNE" then "101"
+      when "JLE" then "110"
+      when "JMP" then "111"
+      else raise CodeSyntaxError
+      end
     end
   end
 end
--- nand2tetris/nand2tetris/projects/06/Assembler/spec/code_spec.rb
+++ nand2tetris/nand2tetris/projects/06/Assembler/spec/code_spec.rb
@@ -2,14 +2,70 @@ require_relative './../lib/code.rb'
 
 RSpec.describe Code do
   describe "dest" do
-    it { expect(Code.dest("111")).to eq "000" }
+    context "normal case" do
+      it { expect(Code.dest(nil)).to eq "000" }
+      it { expect(Code.dest("M")).to eq "001" }
+      it { expect(Code.dest("D")).to eq "010" }
+      it { expect(Code.dest("MD")).to eq "011" }
+      it { expect(Code.dest("A")).to eq "100" }
+      it { expect(Code.dest("AM")).to eq "101" }
+      it { expect(Code.dest("AD")).to eq "110" }
+      it { expect(Code.dest("AMD")).to eq "111" }
+      it { expect { Code.dest("INVALID") }.to raise_error Code::CodeSyntaxError }
+    end
   end
 
   describe "comp" do
-    it { expect(Code.comp("111")).to eq "0000000" }
+    context "A is specified in mnemonic" do
+      it { expect(Code.comp("0")).to eq "0101010" }
+      it { expect(Code.comp("1")).to eq "0111111" }
+      it { expect(Code.comp("-1")).to eq "0111010" }
+      it { expect(Code.comp("D")).to eq "0001100" }
+      it { expect(Code.comp("A")).to eq "0110000" }
+      it { expect(Code.comp("!D")).to eq "0001101" }
+      it { expect(Code.comp("!A")).to eq "0110001" }
+      it { expect(Code.comp("-D")).to eq "0001111" }
+      it { expect(Code.comp("-A")).to eq "0110011" }
+      it { expect(Code.comp("D+1")).to eq "0011111" }
+      it { expect(Code.comp("A+1")).to eq "0110111" }
+      it { expect(Code.comp("D-1")).to eq "0001110" }
+      it { expect(Code.comp("A-1")).to eq "0110010" }
+      it { expect(Code.comp("D+A")).to eq "0000010" }
+      it { expect(Code.comp("D-A")).to eq "0010011" }
+      it { expect(Code.comp("A-D")).to eq "0000111" }
+      it { expect(Code.comp("D&A")).to eq "0000000" }
+      it { expect(Code.comp("D|A")).to eq "0010101" }
+      it { expect { Code.comp(nil) }.to raise_error Code::CodeSyntaxError }
+      it { expect { Code.comp("invalid") }.to raise_error Code::CodeSyntaxError }
+    end
+
+    context "M is specified in mnemonic" do
+      it { expect(Code.comp("M")).to eq "1110000" }
+      it { expect(Code.comp("!M")).to eq "1110001" }
+      it { expect(Code.comp("-M")).to eq "1110011" }
+      it { expect(Code.comp("M+1")).to eq "1110111" }
+      it { expect(Code.comp("M-1")).to eq "1110010" }
+      it { expect(Code.comp("D+M")).to eq "1000010" }
+      it { expect(Code.comp("D-M")).to eq "1010011" }
+      it { expect(Code.comp("M-D")).to eq "1000111" }
+      it { expect(Code.comp("D&M")).to eq "1000000" }
+      it { expect(Code.comp("D|M")).to eq "1010101" }
+      it { expect { Code.comp(nil) }.to raise_error Code::CodeSyntaxError }
+      it { expect { Code.comp("invalid") }.to raise_error Code::CodeSyntaxError }
+    end
   end
 
   describe "jump" do
-    it { expect(Code.jump("111")).to eq "000" }
+    context "normal case" do
+      it { expect(Code.jump(nil)).to eq "000" }
+      it { expect(Code.jump("JGT")).to eq "001" }
+      it { expect(Code.jump("JEQ")).to eq "010" }
+      it { expect(Code.jump("JGE")).to eq "011" }
+      it { expect(Code.jump("JLT")).to eq "100" }
+      it { expect(Code.jump("JNE")).to eq "101" }
+      it { expect(Code.jump("JLE")).to eq "110" }
+      it { expect(Code.jump("JMP")).to eq "111" }
+      it { expect { Code.jump("INVALID") }.to raise_error Code::CodeSyntaxError }
+    end
   end
 end
