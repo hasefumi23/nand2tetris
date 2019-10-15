5e507377a69b3890ed5860dc862c22eeed8190e8
hasefumi23 <hasefumi23@gmail.com>
2019-10-16 00:49:49 +0900

    ある程度のtokenizeに成功

--- nand2tetris/nand2tetris/projects/10/COMPILER/lib/jack_analyzer.rb
+++ nand2tetris/nand2tetris/projects/10/COMPILER/lib/jack_analyzer.rb
@@ -8,8 +8,10 @@ class JackAnalyzer
     tokenizer = JackTokenizer.new(file_name)
     while tokenizer.has_more_tokens?
       tokenizer.advance
-      token = tokenizer.curren_token
-      ["{", "}", ";"].include?(token) ? (puts token) : (print " #{token}")
+      token = tokenizer.current_token
+      # ["{", "}", ";"].include?(token) ? (puts " #{token}") : (print " #{token}")
+      token_type = tokenizer.token_type
+      puts("#{token_type}: #{token}")
     end
   end
 end
--- nand2tetris/nand2tetris/projects/10/COMPILER/lib/jack_tokenizer.rb
+++ nand2tetris/nand2tetris/projects/10/COMPILER/lib/jack_tokenizer.rb
@@ -4,7 +4,8 @@ require 'pry'
 class JackTokenizer
   class CommentError < StandardError; end
 
-  attr_accessor :curren_token
+  attr_accessor :current_token
+  DEBUG = false
 
   KEY_WORDS = %w[class constructor function method field static var int char boolean void true false null this let do if else while return]
   SYMBOLS = %w[{ } ( ) [ ] . , ; + - * / & | < > = ~]
@@ -15,78 +16,117 @@ class JackTokenizer
   # ただし複数行に渡るコメントは未削除
   def initialize(file_name)
     @io = File.open(file_name)
-    # while line = @io.gets
-    #   strings = line.split('')
-    #   p strings
-    # end
     @line = nil
     @current_line = ''
-    @line_pistion = 0
-    @current_words = []
+    @chars = []
+    # @current_words = []
     # 単純に空白文字でsplitした文字列
-    @current_chars = []
-    @splitted_words = []
+    # @splitted_words = []
     # 空白では区切れない一つ以上のトークンを含んだ文字列
     # ex) (a+b-c)など
-    @current_word = ''
+    # @current_word = ''
     # 最小のトークン
-    @curren_token = nil
+    @current_token = nil
   end
 
   # 入力にまだトークンは存在するか？
-  # @curren_token.nil? == true はまだ一度もadvanceが呼ばれていないことを意味する
+  # @current_token.nil? == true はまだ一度もadvanceが呼ばれていないことを意味する
   # @next_token.nil? == false は次のトークンが存在することを意味する
   def has_more_tokens?
     true unless @io.eof?
+  end
+
+  def initial_parse
+    begin
+      return nil if @io.eof?
+      @line = @io.gets.strip
+      @line.gsub!(Regexp.new("//.*"), "")
+      p "@line: #{@line}" if DEBUG
+    end while @line.nil? || @line.empty?
+
+    @line.strip.split("")
   end
 
   # 入力から次のトークンを取得し、それを現在のトークン（現トークン）とする
   # このルーチンは、hasMoreTokens()がtrueの場合のみ呼び出すことができる
   # また、最初は現トークンは設定されていない
   def tokenize
-    # class Main
-    if @splitted_words.empty? && @current_words.empty?
-      begin
-        return nil if @io.eof?
-        @line = @io.gets.strip
-        # if @line == "let i = i | j;"
-        #   binding.pry
-        # end
-        next if @line.start_with?("//")
-      end while @line.nil? || @line.empty?
-
-      @line.gsub!(Regexp.new("//.*"), "")
-      return nil if @line.empty?
-      
-      # コメントは扱いが厄介なのでsplitで分割できるように空白を差し込む
-      @current_words = @line.gsub(Regexp.union(%w[/** /* //]), ' \& ').split
+    if @chars.empty?
+      @chars = initial_parse
     end
 
-    temp_word = @current_words.shift
-    return nil if treat_comment_part(temp_word)
-    # /** や /* によるコメント部分だったらスキップする
-    return nil if @in_comment_part
+    @current_token = tokenize_line
+    @current_token
+  end
 
-    if @splitted_words.empty?
-      # コメント部分のスキップ後にKEY_WORDとSYMBOLを分解する
-      @splitted_words = temp_word.gsub(Regexp.union(KEY_WORDS), ' \& ')
-                  .gsub(Regexp.union(SYMBOLS), ' \& ')
-                  .split
+  def tokenize_line
+    word = ""
+    in_string_constant = false
+    tokened = false
+
+    while true
+      # "test;"のようなIDENTIFIERとSYMBOLの組み合わせを判定する
+      if !word.empty? && SYMBOLS.include?(@chars[0])
+        return word
+      end
+
+      char = @chars.shift
+
+      if char == " "
+        # 空文字が来た場合、可能な限り取り除く
+        while @chars[0] == " "
+          char = @chars.shift
+        end
+
+        if word.empty?
+          next
+        end
+        # 空白の区切りは一つのトークンの区切りなのでリターンする
+        return word
+      end
+
+      word += char
+      if word.start_with?('"') && !word.end_with?('"')
+        # STRING_CONSTANTはここでまとめて処理する
+        begin
+          char = @chars.shift
+          word += char
+        end until char == '"'
+        tokened = true
+      end
+
+      if word == "/" && @chars[0] == "*"
+        local_word = ""
+        local_char = @chars.shift
+
+        # 複数行コメントの終わりまで読み込みを進める
+        until local_word.end_with?("*/")
+          if @chars.empty?
+            @chars = initial_parse
+          end
+          local_char = @chars.shift
+          local_word += local_char
+        end
+
+        if @chars.empty?
+          # 複数行のコメントの場合@charsが空になる可能性があるので空だったら新しい行を取得する
+          @chars = initial_parse
+        end
+        # whileのループ内にいるのでwordを初期化して再出発
+        word = @chars.shift
+        next
+      end
+
+      if SYMBOLS.include?(char)
+        # シンボルは一文字でかつ空白が入るとは限らないので見つけ次第即時リターン
+        return char
+      end
     end
 
-    @word = @splitted_words.shift
-    if SYMBOLS.any? { |sym| @word.start_with?(sym) }
-      @curren_token = @word
-      return @curren_token
-    end
+    word
+  end
 
-    if KEY_WORDS.include?(@word)
-      @curren_token = @word
-      return @curren_token
-    end
-
-    @curren_token = @word
-    # nil
+  def tokened?(word)
   end
 
   def advance
@@ -116,7 +156,19 @@ class JackTokenizer
   # KEYWORD、 SYMBOL、 IDENTIFIER、 INT_CONST、 STRING_CONST
   # 現トークンの種類を返す
   def token_type
-    'token'
+    if KEY_WORDS.include?(@current_token)
+      "KEYWORD"
+    elsif SYMBOLS.include?(@current_token)
+      "SYMBOL"
+    elsif @current_token.start_with?('"') && @current_token.end_with?('"')
+      "STRING_CONST"
+    elsif @current_token =~ /^\d+$/
+      "INT_CONST"
+    elsif @current_token =~ /^\w+$/
+      "IDENTIFIER"
+    else
+      raise StandardError.new("Unexpected token error: token = #{@current_token}")
+    end
   end
 
   # 現トークンのキーワードを返す。こ のルーチンは、tokenType()が KEYWORDの場合のみ呼び出すこと ができる
