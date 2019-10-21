9bbf6a9668cb52157beee041abf3041909efd532
hasefumi23 <hasefumi23@gmail.com>
2019-10-21 11:34:04 +0900

    CompilcationEngineをJackAnalyzerに適用
    - print_tokens_onlyを使ってtokenのみを出力する処理を別メソッドに分離

--- nand2tetris/nand2tetris/projects/10/COMPILER/lib/jack_analyzer.rb
+++ nand2tetris/nand2tetris/projects/10/COMPILER/lib/jack_analyzer.rb
@@ -1,25 +1,32 @@
 #!/usr/bin/env ruby
 
 require_relative "./jack_tokenizer"
+require_relative "./compilation_engine"
 
 class JackAnalyzer
-  def self.run
+  def run
     # file_name = File.expand_path(File.basename(ARGV[0]))
     file_name = File.expand_path(ARGV[0])
-    tokenizer = JackTokenizer.new(file_name)
+    @tokenizer = JackTokenizer.new(file_name)
+    engine = CompilationEngine.new(@tokenizer)
+    engine.compile_class
+    # print_tokens_only
+  end
+
+  def print_tokens_only
     puts("<tokens>")
-    while tokenizer.has_more_tokens?
-      val_for_judge = tokenizer.advance
+    while @tokenizer.has_more_tokens?
+      val_for_judge = @tokenizer.advance
       # ここでnilが返ってくることはファイルの最後に空行が続くことを意味するのでループを抜ける
       break if val_for_judge.nil?
-      token = tokenizer.current_token
+      token = @tokenizer.current_token
       next if token.nil?
 
-      token_type = tokenizer.token_type
+      token_type = @tokenizer.token_type
       puts("<#{token_type}> #{token} </#{token_type}>")
     end
     puts("</tokens>")
   end
 end
 
-# JackAnalyzer.run
+JackAnalyzer.new.run
